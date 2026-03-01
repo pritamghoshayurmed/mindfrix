5c51e802bcfb579faed31cb093bbeaba5959b3cc
Soumyashis Karmakar <soumyashisk@gmail.com>
2026-03-01 17:14:54 +0530

    fix: replace MetaPixel component with inline script for tracking integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { SmoothScroll } from "@/components/SmoothScroll";
 import Script from "next/script";
-import MetaPixel from "@/components/MetaPixel";
 
 const siteUrl = "https://www.mindfrix.com";
 
@@ -215,9 +214,33 @@ export default function RootLayout({
             __html: JSON.stringify(localBusinessSchema),
           }}
         />
-        <MetaPixel pixelId="942188141599836" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '942188141599836');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
       </head>
       <body>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=942188141599836&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         <SmoothScroll />
         {children}
       </body>
